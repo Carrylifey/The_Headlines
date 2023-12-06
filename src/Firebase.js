6694c4921e8cecfcc3e661c4734bb18438118821
Subhankar Saha <subhankarsahacse9@gmail.com>
2023-12-06 07:21:17 +0530

    added dbcollection & favorite arictle

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,6 +16,19 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-
-export  { auth };
+export const setUserFavorites = async (userId, favorites) => {
+  try {
+    const userRef = doc(db, "users", userId);
+    await setDoc(userRef, { favorites });
+    console.log("Favorites updated successfully");
+  } catch (error) {
+    console.error("Error updating favorites:", error);
+  }
+};
+export const getUserFavorites = async (userId) => {
+  const userDoc = await getDoc(doc(db, "users", userId));
+  return userDoc.exists() ? userDoc.data().favorites : [];
+};
+export { auth, db };
--- src/News.js
+++ src/News.js
@@ -1,8 +1,37 @@
 // NewsDetailsModal.js
 import React from "react";
+import { useState, useEffect } from "react";
+import { auth, setUserFavorites, getUserFavorites } from "./Firebase";
 
 const News = ({ article, onClose }) => {
-  
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  useEffect(() => {
+    // Check if the current article is in the user's favorites
+    const checkFavoriteStatus = async () => {
+      const userId = auth.currentUser?.uid;
+      if (userId) {
+        const favorites = await getUserFavorites(userId);
+        setIsFavorite(favorites.includes(article.id));
+      }
+    };
+
+    checkFavoriteStatus();
+  }, [article.id]);
+
+  const handleToggleFavorite = async () => {
+    const userId = auth.currentUser?.uid;
+
+    if (userId) {
+      const favorites = await getUserFavorites(userId);
+      const updatedFavorites = isFavorite
+        ? favorites.filter((favId) => favId !== article.id)
+        : [...favorites, article.id];
+
+      await setUserFavorites(userId, updatedFavorites);
+      setIsFavorite(!isFavorite);
+    }
+  };
   return (
     <div className="fixed top-0 left-0 w-full h-full flex items-center justify-center">
       <div className="bg-white p-8 max-w-2xl max-h-full overflow-y-auto">
@@ -21,6 +50,14 @@ const News = ({ article, onClose }) => {
         >
           Read Full Article
         </a>
+        <button
+          onClick={handleToggleFavorite}
+          className={`mt-4 ${
+            isFavorite ? "bg-red-500" : "bg-blue-500"
+          } hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+        >
+          {isFavorite ? "Remove from Favorites" : "Add to Favorites"}
+        </button>
         <button
           onClick={onClose}
           className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
